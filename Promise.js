dcb4f048076a43a1eae4554e0a279044fda1bbfd
Mayank-TN <mtandon10@gmail.com>
2022-10-30 16:27:13 +0530

    promiseAll

--- Promise.js
+++ Promise.js
@@ -28,7 +28,7 @@ function createPost(post){
         setTimeout(()=>{posts.push({...post , createdAt : new Date().getTime()})
             const error = false ;
         if(!error){
-            resolve()
+            resolve(posts)
         }
         else{
             reject('Something went wrong')
@@ -52,23 +52,58 @@ function deletePost(){
         })
 }
 
-createPost({title : 'Post Three' , body : 'This is post three'}).then(
-    ()=>{
-        getPosts()
-        deletePost().then((ele)=>{
-            console.log(ele)
-        getPosts()
-        deletePost().then((ele)=>{
-            console.log(ele)
-            getPosts()
-            deletePost().then((ele)=>{
-                console.log(ele)
-                getPosts()
-                deletePost().then((ele)=>{
-                    console.log(ele)
-                    getPosts()
-                }).catch(err=>console.log(err))
-            }
-        )})
-    })}
-    )
+// createPost({title : 'Post Three' , body : 'This is post three'}).then(
+//     ()=>{
+//         getPosts()
+//         deletePost().then((ele)=>{
+//             console.log(ele)
+//         getPosts()
+//         deletePost().then((ele)=>{
+//             console.log(ele)
+//             getPosts()
+//             deletePost().then((ele)=>{
+//                 console.log(ele)
+//                 getPosts()
+//                 deletePost().then((ele)=>{
+//                     console.log(ele)
+//                     getPosts()
+//                 }).catch(err=>console.log(err))
+//             }
+//         )})
+//     })}
+//     )
+
+
+//Promise.all
+// const promise1 = Promise.resolve('Hello')
+// const promise2 = 10;
+// const promise3 = new Promise((resolve,reject)=>{
+//    setTimeout(resolve , 2000 , 'OK')
+// })
+
+const user = {
+    name : 'Yash',
+    lastActivityTime : new Date().getTime()
+}
+
+const updateLastUserActivityTime = ()=>{
+    return new Promise((resolve ,reject)=>{
+       setTimeout( ()=>{user.lastActivityTime = new Date().getTime()
+        resolve(user.lastActivityTime)}
+
+       ,1000)
+    })
+}
+
+function userUpdatePost(){
+    Promise.all([createPost({title : 'Post Three' , body : 'This is post three'}) ,updateLastUserActivityTime()]).then(([createPostResolve,updateLastUserActivityTimeResolve])=>{
+        console.log(createPostResolve)
+        console.log(updateLastUserActivityTimeResolve)
+    }).catch(err=> console.log(err))
+}
+
+userUpdatePost()
+
+
+
+
